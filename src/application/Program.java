c79b167abfee5b5dfe3fd9673f7d6f22df284796
Professora Diana <94633813+Prof-Diana@users.noreply.github.com>
2022-08-02 18:46:10 -0300

    Create Program.java

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.SegueMouse;
+
+  public class Program {
+
+    public static void main(String [] args){
+   
+       SegueMouse seguidor = new SegueMouse();
+
+    }
+  }
